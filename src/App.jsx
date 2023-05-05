d468e406b1b1761553d081df7e2644d4ea97e40b
AmiltoJ <Amiltosmaniotto@gmail.com>
2023-05-05 15:23:19 -0300

    commit final

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import style from "./App.module.css";
+
+import { CardPerson } from "./components/CardPerson";
+
+export function App() {
+  const listStack = [
+    { Tec: "JavaScript", color: "#3d3d3d" },
+    { Tec: "HTML" },
+    { Tec: "CSS" },
+    { Tec: "Typescript" },
+    { Tec: "React" },
+    { Tec: "C++" },
+  ];
+
+  return (
+    <div className={style.container}>
+      <CardPerson
+        cover="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50"
+        avatar="https://github.com/AmiltoJ.png"
+        name="Amilto J S"
+        office="Web Fracasso"
+        stacks={listStack}
+      />
+    </div>
+  );
+}
